b3fe58af1ce87c975555b393dbc5f5ce66707243
Tomas Ketlerius <tomas.ketlerius@teltonika.lt>
2024-03-16 18:35:09 +0200

    integrate esp-adf

--- components/bites_i2s_stream/bites_i2s_stream.c
+++ components/bites_i2s_stream/bites_i2s_stream.c
@@ -6,7 +6,6 @@
 #include "freertos/event_groups.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
-#include "nvs_flash.h"
 #include "sdkconfig.h"
 #include "audio_element.h"
 #include "audio_pipeline.h"
@@ -26,21 +25,8 @@ static const char *TAG = "HTTP_MP3_EXAMPLE";
 
 #define STREAM_URI "http://stream.palanga.live/palanga128.mp3"
 
-void app_main(void)
+void i2s_stream_main(void)
 {
-    esp_err_t err = nvs_flash_init();
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
-        // NVS partition was truncated and needs to be erased
-        // Retry nvs_flash_init
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        err = nvs_flash_init();
-    }
-#if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 1, 0))
-    ESP_ERROR_CHECK(esp_netif_init());
-#else
-    tcpip_adapter_init();
-#endif
-
     audio_pipeline_handle_t pipeline;
     audio_element_handle_t http_stream_reader, i2s_stream_writer, mp3_decoder;
 
